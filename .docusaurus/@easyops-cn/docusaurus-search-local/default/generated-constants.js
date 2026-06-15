f33a5f1a1fbf109c229c55fed947a3eca995d53c
kimhyunjin <kim.hyunjin@jinnhyun.com>
2026-06-15 14:15:27 +0900

    second commit

---
+++ .docusaurus/@easyops-cn/docusaurus-search-local/default/generated-constants.js
@@ -0,0 +1,6 @@
+import lunr from "/home/jnh/workspace/dev_wiki/node_modules/lunr/lunr.js";
+export const removeDefaultStopWordFilter = [];
+export const language = ["en"];
+export const searchIndexUrl = "search-index{dir}.json?_=71d939e0";
+export const searchResultLimits = 8;
+export const fuzzyMatchingDistance = 1;
